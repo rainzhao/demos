b0df9f97381427009c3df1d0cbbb5ccc3307ee61
edz <edz@edzdeMacBook-Pro.local>
2017-01-15 00:48:40 +0800

    publish to github pages

---
+++ publish.js
@@ -0,0 +1,7 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+ghpages.publish(path.join(__dirname, 'dist'), function(err) {
+  if (err) return console.log(err);
+  console.log('publish success');
+});
